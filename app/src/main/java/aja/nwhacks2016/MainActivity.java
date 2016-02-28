fc00002f23c922d4eb6f32395e726f01c205b1c2
adrianclim <aclim@sfu.ca>
2016-02-27 21:38:36 -0800

    Added listeners for keyboard

--- app/src/main/java/aja/nwhacks2016/MainActivity.java
+++ app/src/main/java/aja/nwhacks2016/MainActivity.java
@@ -4,14 +4,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
+import aja.rhythm.Player;
 
 public class MainActivity extends ActionBarActivity {
+    private Player player = Player.getPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setAllKeyboardListeners();
     }
 
 
@@ -35,5 +40,40 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setAllKeyboardListeners(){
+        int[] buttonids = {R.id.imageButton,R.id.imageButton2,R.id.imageButton3,
+            R.id.imageButton4,R.id.imageButton5,R.id.imageButton6,
+            R.id.imageButton7,R.id.imageButton8,R.id.imageButton9,
+            R.id.imageButton10,R.id.imageButton11,R.id.imageButton12};
+
+        for (int row=0;row<3;row++){
+            for (int col=0;col<4;col++){
+                int subdivision;
+                if (row<2){
+                    subdivision = 4;
+                    setKeyboardButtonListener(buttonids[row*4+col],subdivision,row*4+col);
+                }
+                else if (col!=0){
+                    subdivision = 3;
+                    setKeyboardButtonListener(buttonids[row*4+col],subdivision,col-1);
+                }
+                else{
+                    //Add tie function
+                }
+            }
+        }
+    }
+
+    private void setKeyboardButtonListener(int buttonId,final int subdivision,final int beatpattern){
+        ImageButton button = (ImageButton)findViewById(buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println(beatpattern);
+                player.addBeat(beatpattern,subdivision);
+            }
+        });
     }
 }
